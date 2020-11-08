f4c52357b2ad90c70e291b95ebf595e62e709f16
Kardos Péter <kp@metnet.hu>
2020-11-08 18:10:59 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-# v1.2 2020.11.08.
+# v1.3 2020.11.08.
 
 export PYTHONPATH="/home/pi/import"
 
 while true; do
     pkill task01.py
-    /home/pi/rpi_station/task01.py &
+    /home/pi/rpi_station/task01.py & >/dev/null 2>&1 
     sleep 600
 done
 
