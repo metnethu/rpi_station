dcd32b7d9a08005f60e42be46d8aa6d831d891af
Kardos Péter <kp@metnet.hu>
2020-11-10 20:31:53 +0100

    Update task01.py
    Pressure vs. pressure bug

--- task01.py
+++ task01.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
           print ("Temperature : ", t1, "C")
           print ("Pressure : ", p, "hPa")
           print ("Humidity : ", rh, "%")
-          post_data['Pressure']=p
+          post_data['pressure']=p
         except:
           print("There is no i2c temperature")
 
