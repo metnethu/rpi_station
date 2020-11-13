12b771c4b4e04dfb06cc93ebab1489490cfcc06f
Kardos Péter <kp@metnet.hu>
2020-11-13 19:54:15 +0100

    Update task01.py
    Pressure bug, pressure round

--- task01.py
+++ task01.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 # "DATASHEET": http://cl.ly/ekot
 # https://gist.github.com/kadamski/92653913a53baf9dd1a8
-# V1.1 2020.11.12
+# V1.2 2020.11.13
 from __future__ import print_function
 import serial, struct, sys, time, subprocess, requests, bme280, Adafruit_DHT
 
